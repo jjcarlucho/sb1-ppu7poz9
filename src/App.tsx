ddc023765216c3e337488302e99c0fec54a8bced
jjcarlucho <jjcarlucho@gmail.com>
2025-04-24 18:18:09 -0400

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import VSL from './components/VSL';
 import Hero from './components/Hero';
 import Story from './components/Story';
 import Benefits from './components/Benefits';
 import Decision from './components/Decision';
 import Pricing from './components/Pricing';
-import FinalCTA from './components/FinalCTA.tsx';
+import FinalCTA from './components/FinalCTA';
 import Footer from './components/Footer';
 
 const App: React.FC = () => {
   return (
     <div className="w-full">
+      <VSL />
       <Hero />
       <Story />
       <Benefits />
